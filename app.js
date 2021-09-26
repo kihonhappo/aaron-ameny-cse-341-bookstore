c8d8edb47edf7762b05c5191672339805e2321f9
kihonhappo <41194905+kihonhappo@users.noreply.github.com>
2021-09-25 22:12:35 -0500

    fsfs

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const rootDir = require('./util/path');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
